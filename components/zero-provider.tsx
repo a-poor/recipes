a0e1658ec8abcbbe184212ec7407e7755422c55e
Austin Poor <45295232+a-poor@users.noreply.github.com>
2025-01-23 14:04:39 -0800

    Fixes zero provider error
    There was an error where the zero client was causing an error
    because it was running on the server side (and was trying to
    access IndexedDB).

--- components/zero-provider.tsx
+++ components/zero-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useState, useEffect } from 'react';
 import { Zero } from '@rocicorp/zero';
 import { useZero as _useZero, ZeroProvider as ZeroProviderBase } from '@rocicorp/zero/react';
 import { schema } from '@/schema';
@@ -12,13 +12,23 @@ export const ZeroProvider = ({ children, userID, auth }: {
   userID: string;
   auth: string;
 }) => {
-  const z = useMemo(() => new Zero({
-    userID,
-    auth,
-    server: process.env.NEXT_PUBLIC_CACHE_SERVER,
-    schema,
-    kvStore: "mem", // "idb"
-  }), [userID, auth]);
+  const [z, setZ] = useState<Zero<Schema> | null>(null);
+  useEffect(() => {
+    setZ(new Zero({
+      userID,
+      auth,
+      server: process.env.NEXT_PUBLIC_CACHE_SERVER,
+      schema,
+      kvStore: "mem", // "idb"
+    }))
+  }, [userID, auth]);
+  if (!z) {
+    return (
+      <>
+        {children}
+      </>
+    );
+  };
   return (
     <ZeroProviderBase zero={z}>
       {children}
